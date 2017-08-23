f2d8aaf8103323b27967fdd5212abec8c542472c
Joey <joey@xandem.com>
2017-08-22 23:12:38 -0600

    minor.

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-#Joey Wilson
+"""Joey Wilson - messing around with hardrock 100 data to hopefully learn from it."""
+
 
 import pandas as pd
 import matplotlib as mpl
 import scipy as sp
 
-data = pd.read_csv('hardrock-elapsed-2017-08-15.csv')
+data = pd.read_csv('data/hardrock-elapsed-2017-08-15.csv')
+
+#Find runners that finished in the bottom half early in the race
+data_early = data.sort_values("Maggie In")
+print data_early
 
 
