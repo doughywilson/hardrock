fc8a9136aa71ece6ce67c6cb27968c6512440c68
Joey Wilson <joey@xandem.com>
2017-08-24 15:53:48 -0600

    Added more data files. Plotting ALL years at once now.

--- main.py
+++ main.py
@@ -5,22 +5,28 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import scipy as sp
 import df_funcs as dff
+import glob
 
-data = pd.read_csv('data/2013.csv')
+filenames = glob.glob('data/2*')
 
-# Get all timestamps into timedeltas
-dff.change_in_out_to_timedelta(data)
+for fn in filenames:
 
-#Plot first half duration vs last half duration for each runner
-firstHalf = data["Engineer In"]
-secondHalf = data["Finish"] - firstHalf
-place = data["Overall rank"]
+    data = pd.read_csv(fn)
+
+    # Get all timestamps into timedeltas
+    dff.change_in_out_to_timedelta(data)
+
+    #Plot first half duration vs last half duration for each runner
+    firstHalf = data["Engineer In"]
+    secondHalf = data["Finish"] - firstHalf
+    place = data["Overall rank"]
 
 
-plt.plot(firstHalf, secondHalf, '.')
-for k in range(firstHalf.size):
-    if firstHalf[k] < 100 and secondHalf[k] < 100:
-        plt.text(firstHalf[k], secondHalf[k], place[k])
+    plt.plot(firstHalf, secondHalf, '.')
+    # for k in range(firstHalf.size):
+    #     if firstHalf[k] < 100 and secondHalf[k] < 100:
+    #         plt.text(firstHalf[k], secondHalf[k], place[k])
+
 plt.axis([10, 30, 10, 30])
 plt.plot([0,100],[0,100],":")
 plt.xlabel('1st Half time (h)')
